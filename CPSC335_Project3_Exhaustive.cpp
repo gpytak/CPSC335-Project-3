3893fcbde9899d48ae2aad9208e8336962153805
Gregory <85207059+gpytak@users.noreply.github.com>
2022-11-03 13:42:33 -0700

    Add files via upload

---
+++ CPSC335_Project3_Exhaustive.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long fibonacci(unsigned long long int n_val){
+    if(n_val <= 1){
+        return n_val;
+    }else{
+        return fibonacci(n_val-1) + fibonacci(n_val-2);
+    }
+}
+
+int main(){
+    unsigned long long int input = 15;
+    unsigned long long int output;
+
+    output = fibonacci(input);
+
+    cout << "\nThe 15th element of fibonacci is " << output << endl;
+    return 0;
+}
